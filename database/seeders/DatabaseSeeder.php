ff6fc3b8c66f962efad5606e2eefeecc334e1b22
ZuanPdana <pradanazuan28+github2@gmail.com>
2026-07-29 07:37:18 +0700

    Revert "Admin page"

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            AdminSeeder::class,
             CategorySeeder::class,
             PaymentMethodSeeder::class,
             CouponSeeder::class,
